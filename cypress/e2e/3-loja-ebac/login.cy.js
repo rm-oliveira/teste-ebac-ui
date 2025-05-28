8de05f853415fd635da67d5a707e990fda49a5ff
Rayane Oliveira <rayanemoliveira22@gmail.com>
2025-05-28 19:11:39 -0300

    teste login com massa de dados e fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,14 +1,16 @@
 /// <reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    })
+        cy.visit('minha-conta')
+    });
 
     afterEach(() => {
         cy.screenshot()
-    })
+    });
 
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('rayane@ebac.com')
@@ -17,7 +19,7 @@ describe('Funcionalidade: Login', () => {
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
             .should('contain', 'Olá, rayane (não é rayane? Sair)')
-    })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
         cy.get('#username').type('ray@ebac.com')
@@ -27,7 +29,7 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error')
             .should('contain', 'Endereço de e-mail desconhecido.')
         cy.get('.woocommerce-error').should('exist')
-    })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('rayane@ebac.com')
@@ -36,6 +38,24 @@ describe('Funcionalidade: Login', () => {
 
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail rayane@ebac.com está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+            .should('contain', 'Olá, rayane (não é rayane? Sair)')
+    });
+
+    it.only('Deve fazer login com sucesso - Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+                .should('contain', 'Olá, rayane (não é rayane? Sair)')
+        })
     });
 
 })
